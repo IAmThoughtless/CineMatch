0ddee830646d3de29c43895554b0060692123f51
christoforosboulmpos <christoforosbls@gmail.com>
2025-11-23 20:23:53 +0200

    Top10ConsoleCommit

--- src/main/java/com/example/cinematch/HelloApplication.java
+++ src/main/java/com/example/cinematch/HelloApplication.java
@@ -32,22 +32,20 @@ public class HelloApplication extends Application {
         logoLabel.setEffect(shadow);
 
         Button homeBtn = new Button("Home Page");
-        homeBtn.setOnAction(event -> {showHomeView();});
         homeBtn.setStyle("-fx-background-color: transparent; -fx-text-fill: white; -fx-font-size: 14px; -fx-font-weight: bold; -fx-cursor: hand;");
-        makeButtonAnimated(homeBtn, false);
+
 
         Button top10Btn = new Button("Top 10");
         top10Btn.setStyle("-fx-background-color: transparent; -fx-text-fill: white; -fx-font-size: 14px; -fx-font-weight: bold; -fx-cursor: hand;");
-        makeButtonAnimated(top10Btn, false );
+
 
         Button loginBtn = new Button("Login / Register");
-        loginBtn.setOnAction(event -> {showLoginView();});
         loginBtn.setStyle("-fx-background-color: #E50914; -fx-text-fill: white; -fx-font-weight: bold; -fx-cursor: hand; -fx-background-radius: 5;");
-        makeButtonAnimated(loginBtn, true);
+
 
         Button QuizBtn = new Button("Quiz");
         QuizBtn.setStyle("-fx-background-color: transparent; -fx-text-fill: white; -fx-font-size: 14px; -fx-font-weight: bold; -fx-cursor: hand; ");
-        makeButtonAnimated(QuizBtn, false);
+
 
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
@@ -67,136 +65,6 @@ public class HelloApplication extends Application {
         primaryStage.setTitle("CineMatch App");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        showHomeView();
     }
-
-    private void showHomeView() {
-        Label welcomeLabel = new Label("Welcome to CineMatch");
-        welcomeLabel.setStyle("-fx-text-fill: white; -fx-font-size: 36px; -fx-font-weight: bold;");
-
-        Label subTitle = new Label("Search for your favourite movie or actor/actress");
-        subTitle.setStyle("-fx-text-fill: #cccccc; -fx-font-size: 18px;");
-
-        TextField searchField = new TextField();
-        searchField.setPromptText("Search...");
-        searchField.setPrefSize(300, 40);
-        searchField.setStyle("-fx-background-color: white; -fx-font-size: 14px; -fx-background-radius: 20 0 0 20; -fx-padding: 0 15;");
-
-        Button searchButton = new Button("Search");
-        searchButton.setPrefSize(120, 40);
-        searchButton.setStyle("-fx-background-color: #E50914; -fx-text-fill: white; -fx-font-weight: bold; -fx-cursor: hand; -fx-background-radius: 0 20 20 0;");
-
-
-        HBox searchBox = new HBox(0, searchField, searchButton);
-        searchBox.setAlignment(Pos.CENTER);
-        VBox.setMargin(searchBox, new Insets(30, 0, 0, 0));
-
-        VBox homeContent = new VBox(10, welcomeLabel, subTitle, searchBox);
-        homeContent.setAlignment(Pos.CENTER);
-
-        root.setCenter(homeContent);
-    }
-    private void showLoginView() {
-        Label loginTitle = new Label("Sign In");
-        loginTitle.setStyle("-fx-text-fill: white; -fx-font-size: 28px; -fx-font-weight: bold;");
-
-        TextField usernameField = new TextField();
-        usernameField.setPromptText("Email or Username");
-        usernameField.setPrefHeight(40);
-        usernameField.setStyle("-fx-background-radius: 5; -fx-background-color: #333; -fx-text-fill: white;");
-
-        PasswordField passwordField = new PasswordField();
-        passwordField.setPromptText("Password");
-        passwordField.setPrefHeight(40);
-        passwordField.setStyle("-fx-background-radius: 5; -fx-background-color: #333; -fx-text-fill: white;");
-
-        Button signInBtn = new Button("Sign In");
-        signInBtn.setPrefWidth(300);
-        signInBtn.setPrefHeight(40);
-        signInBtn.setStyle("-fx-background-color: #E50914; -fx-text-fill: white; -fx-font-weight: bold; -fx-cursor: hand; -fx-background-radius: 5;");
-        makeButtonAnimated(signInBtn, true);
-
-        Label registerLink = new Label("New to CineMatch? Sign up now.");
-        registerLink.setStyle("-fx-text-fill: #cccccc; -fx-cursor: hand;");
-        registerLink.setOnMouseClicked( event -> {showRegisterView();});
-        registerLink.setOnMouseEntered(e -> registerLink.setStyle("-fx-text-fill: white; -fx-underline: true;"));
-        registerLink.setOnMouseExited(e -> registerLink.setStyle("-fx-text-fill: #cccccc; -fx-underline: false;"));
-
-        VBox loginForm = new VBox(20, loginTitle, usernameField, passwordField, signInBtn, registerLink);
-        loginForm.setAlignment(Pos.CENTER);
-        loginForm.setPadding(new Insets(20, 20, 20, 20));
-        loginForm.setMaxWidth(400);
-        loginForm.setMaxHeight(300);
-        loginForm.setStyle("-fx-background-color: rgba(0, 0, 0, 0.75); -fx-background-radius: 10;");
-
-        root.setCenter(loginForm);
-    }
-    private void showRegisterView() {
-        Label regTitle = new Label("Create Account");
-        regTitle.setStyle("-fx-text-fill: white; -fx-font-size: 28px; -fx-font-weight: bold;");
-
-        TextField emailField = new TextField();
-        emailField.setPromptText("Email Address");
-        emailField.setPrefHeight(40);
-        emailField.setStyle("-fx-background-radius: 5; -fx-background-color: #333; -fx-text-fill: white;");
-
-        TextField userField = new TextField();
-        userField.setPromptText("Username");
-        userField.setPrefHeight(40);
-        userField.setStyle("-fx-background-radius: 5; -fx-background-color: #333; -fx-text-fill: white;");
-
-        PasswordField passField = new PasswordField();
-        passField.setPromptText("Password");
-        passField.setPrefHeight(40);
-        passField.setStyle("-fx-background-radius: 5; -fx-background-color: #333; -fx-text-fill: white;");
-
-        PasswordField confirmPassField = new PasswordField();
-        confirmPassField.setPromptText("Confirm Password");
-        confirmPassField.setPrefHeight(40);
-        confirmPassField.setStyle("-fx-background-radius: 5; -fx-background-color: #333; -fx-text-fill: white;");
-
-        Button registerBtn = new Button("Sign Up");
-        registerBtn.setPrefWidth(300);
-        registerBtn.setPrefHeight(40);
-        registerBtn.setStyle("-fx-background-color: #E50914; -fx-text-fill: white; -fx-font-weight: bold; -fx-cursor: hand; -fx-background-radius: 5;");
-        makeButtonAnimated(registerBtn, true);
-
-        Label loginLink = new Label("Already have an account? Sign in.");
-        loginLink.setStyle("-fx-text-fill: #cccccc; -fx-cursor: hand;");
-        loginLink.setOnMouseClicked(e -> showLoginView());
-        loginLink.setOnMouseEntered(e -> loginLink.setStyle("-fx-text-fill: white; -fx-underline: true;"));
-        loginLink.setOnMouseExited(e -> loginLink.setStyle("-fx-text-fill: #cccccc; -fx-underline: false;"));
-
-        VBox regForm = new VBox(20, regTitle, emailField, userField, passField, confirmPassField, registerBtn, loginLink);
-        regForm.setAlignment(Pos.CENTER);
-        regForm.setPadding(new Insets(40));
-        regForm.setMaxWidth(400);
-        regForm.setStyle("-fx-background-color: rgba(0, 0, 0, 0.75); -fx-background-radius: 10;");
-
-        root.setCenter(regForm);
-    }
-    private void makeButtonAnimated(Button btn, boolean isRedButton) {
-
-        btn.setOnMouseEntered(e -> {
-            btn.setScaleX(1.10);
-            btn.setScaleY(1.10);
-            if (isRedButton) {
-
-                btn.setStyle("-fx-background-color: #ff1f2c; -fx-text-fill: white; -fx-font-weight: bold; -fx-cursor: hand; -fx-background-radius: " + btn.getBackground().getFills().get(0).getRadii().getTopLeftHorizontalRadius() + ";");
-            }
-        });
-
-
-        btn.setOnMouseExited(e -> {
-            btn.setScaleX(1.0);
-            btn.setScaleY(1.0);
-            if (isRedButton) {
-
-                btn.setStyle("-fx-background-color: #E50914; -fx-text-fill: white; -fx-font-weight: bold; -fx-cursor: hand; -fx-background-radius: " + btn.getBackground().getFills().get(0).getRadii().getTopLeftHorizontalRadius() + ";");
-            }
-        });
-    }
-
 }
 
