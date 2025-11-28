7dcac7450e049a32bcf748e105893e612379d200
Orestis <orestis.tottas@gmail.com>
2025-11-28 14:24:46 +0200

    Αδδιτιον οφ αθτη

--- src/main/java/com/cinematch/cinematchbackend/CineMatchBackendApplication.java
+++ src/main/java/com/cinematch/cinematchbackend/CineMatchBackendApplication.java
@@ -3,7 +3,7 @@ package com.cinematch.cinematchbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class CineMatchBackendApplication {
 
     public static void main(String[] args) {
