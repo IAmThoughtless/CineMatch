d8cbbc863a933b9367b85680ffe6d20a4996b7f3
kolia <koliaswhy@gmail.com>
2025-11-28 02:22:41 +0200

    Backend_New_

---
+++ src/main/java/com/cinematch/cinematchbackend/CineMatchBackendApplication.java
@@ -0,0 +1,12 @@
+package com.cinematch.cinematchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineMatchBackendApplication.class, args);
+    }
+}
---
+++ src/main/java/com/cinematch/cinematchbackend/model/User.java
@@ -0,0 +1,10 @@
+package com.cinematch.cinematchbackend.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String email;
+    private String password;
+}
